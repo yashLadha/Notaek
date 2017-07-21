f8603037b892c482d6ca514642ee5e6ae32a7335
yashLadha <yashladhapankajladha123@gmail.com>
2017-07-21 19:18:09 +0530

    Added ajax call and async search
    Signed-off-by: yashLadha <yashladhapankajladha123@gmail.com>

--- note_taking_app/public/javascripts/fuzzyField.js
+++ note_taking_app/public/javascripts/fuzzyField.js
@@ -1,23 +1,33 @@
 // Implement the async fuzzy search
+
+function fuzzyAjaxCall(text) {
+  $.ajax({
+    type: 'GET',
+    url: '/notes/async/'+text,
+    success: function(msg) {
+      console.log('Success: ' + msg);
+    },
+    error: function(request, status, error) {
+      console.log(error);
+    }
+  });
+};
+
 $(function() { // Executes when the document is ready
-    $("#search-bar").keyup(function(){
-        console.log($(this).val());
-        text = $(this).val();
-        $.ajax({
-            type: 'GET',
-            url: '/notes/async/'+text,
-            success: function(msg) {
-                console.log(msg);
-            },
-            error: function(request, status, error) {
-                console.log(error);
-            }
-        });
-    });  
+  $("#search-bar").keyup(function(){
+    console.log($(this).val());
+    text = $(this).val(); // grabs the text inside the search bar
+    // send an ajax call to the api, requesting the contents of posts (fuzzy)
+    var results = fuzzyAjaxCall(text);
+  });
 });
 
+// Finds all the notes that fuzzy search matches
 $(function() {   
-    $('#search').click(function() {
-        console.log('Seatch Button is pressed');
-    });
+  $('#search').click(function() {
+    console.log('Fetching Fuzzy search post');
+    // Text for fuzzy search
+    var text = document.getElementById('search-bar').value;
+    var results = fuzzyAjaxCall(text);
+  });
 });
--- note_taking_app/routes/note_take.js
+++ note_taking_app/routes/note_take.js
@@ -5,95 +5,95 @@ var noteModel = require('../models/note.js');
 
 
 router.get('/createNote', function(req, res) {
-    if (req.user)
-	res.render('note_create', {title: 'Note Create', user: req.user});
-    else
-	res.redirect('/');
+  if (req.user)
+    res.render('note_create', {title: 'Note Create', user: req.user});
+  else
+    res.redirect('/');
 });
 
 router.post('/createNote', function(req, res) {
-    if (req.user) {
-	userInfo = req.user;
-	postTitle = req.body.title;
-	postContent = req.body.content;
+  if (req.user) {
+    userInfo = req.user;
+    postTitle = req.body.title;
+    postContent = req.body.content;
 
-	if (postTitle) {
-	    noteModel.find({author: userInfo._id, title: postTitle}, function(err, note) {
-		if (err)
-		    console.log('Post already present');
-		else {
-		    note = new noteModel({title: postTitle, author: userInfo._id, content: postContent});
-		    note.save(function(err) {
-			if (err)
-			    console.log('Note is not able to save properly');
-			else {
-			    console.log('Note is saved properly');
-			    res.redirect('/notes/createNote');
-			    note.on('es-indexed', function() {
-				console.log('Document is indexed');
-			    });
-			}
-		    });
-		}
-	    });
-	}
+    if (postTitle) {
+      noteModel.find({author: userInfo._id, title: postTitle}, function(err, note) {
+        if (err)
+          console.log('Post already present');
+        else {
+          note = new noteModel({title: postTitle, author: userInfo._id, content: postContent});
+          note.save(function(err) {
+            if (err)
+              console.log('Note is not able to save properly');
+            else {
+              console.log('Note is saved properly');
+              res.redirect('/notes/createNote');
+              note.on('es-indexed', function() {
+                console.log('Document is indexed');
+              });
+            }
+          });
+        }
+      });
     }
+  }
 });
 
 var findFuzzyPosts = function(text, user) {
-    noteModel.search({
-	bool: {
-	    must: {
-		fuzzy: {
-		    title: text // Text for applying fuzzy search algo
-		}
-	    },
-	    filter: {
-		term: {
-		    author: user._id
-		}
-	    }
-	}
-    }, {hydrate: true}, function(err, result) {
-	if (err) {
-	    console.log('Error finding data' + err);
-	} else {
-	    if (result.hits.hits) {
-		console.log(result.hits.hits);
-		return result.hits.hits;
-	    }
-	}
-	return null;
-    });
+  noteModel.search({
+    bool: {
+      must: {
+        fuzzy: {
+          title: text // Text for applying fuzzy search query
+        }
+      },
+      filter: {
+        term: {
+          author: user._id
+        }
+      }
+    }
+  }, {hydrate: true}, function(err, result) {
+    if (err) {
+      console.log('Error finding data' + err);
+    } else {
+      if (result.hits.hits) {
+        console.log(result.hits.hits);
+        return result.hits.hits;
+      }
+    }
+    return null;
+  });
 };
 
 router.get('/async/:text', function(req, res) {
-    user = req.user;
-    if (user) {
-        console.log('Request received: ' + req.params.text);
-	posts = findFuzzyPosts(req.params.text, user);
-    }
+  user = req.user;
+  if (user) {
+    console.log('Request received: ' + req.params.text);
+    posts = findFuzzyPosts(req.params.text, user);
+  }
 });
 
 router.get('/', function(req, res) {
-    if (req.user) {
-	userInfo = req.user;
-	noteModel.search({
-	    match: {
-		author: userInfo._id
-	    }
-	}, {hydrate: true}, function(err, result) {
-	    if(err) {
-		console.log('No data found');
-		res.redirect('/');
-	    } else {
-		console.log(result.hits.hits);
-		res.render('note_index', {notes: result.hits.hits, user: userInfo});
-	    }
-	});
-    }
-    else
-	res.redirect('/');
+  if (req.user) {
+    userInfo = req.user;
+    noteModel.search({
+      match: {
+        author: userInfo._id
+      }
+    }, {hydrate: true}, function(err, result) {
+      if(err) {
+        console.log('No data found');
+        res.redirect('/');
+      } else {
+        console.log(result.hits.hits);
+        res.render('note_index', {notes: result.hits.hits, user: userInfo});
+      }
+    });
+  }
+  else
+    res.redirect('/');
 });
 
 module.exports = router;
